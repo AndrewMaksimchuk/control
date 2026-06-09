0bd5a05d3bb7bce27ff747c9a7b9eb48f6b7e24b
Andrew Maksimchuk <amaksimcthuk@gmail.com>
2026-06-09 22:58:59 +0300

    fix: check user input

--- stdlib.bash
+++ stdlib.bash
@@ -1,13 +1,32 @@
-function EXIT_SUCCESS {
+function EXIT_SUCCESS ()
+{
   exit 0
 }
 
-function EXIT_FAILURE {
+function EXIT_FAILURE ()
+{
   exit 1
 }
 
-function EXIT_PREVIOUS_COMMAND_CODE {
+function EXIT_PREVIOUS_COMMAND_CODE ()
+{
   # combination of Special Parameters and Parameter Expansion
   # ${VARIABLE:-DEFAULT_VALUE}
   exit "${1:-$?}"
 }
+
+function is_numeric ()
+{
+  [[ $1 =~ ^[0-9]+$ ]]
+}
+
+function is_alphanumeric ()
+{
+  [[ $1 =~ ^[A-Za-z0-9]+$ ]]
+}
+
+function is_alphanumeric_dash_underscore ()
+{
+  [[ $1 =~ ^[A-Za-z0-9_-]+$ ]]
+}
+
