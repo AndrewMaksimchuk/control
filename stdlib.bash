b14ec3b2fdc2bb1bae0cca23de22d3c5557cf583
Andrew Maksimchuk <amaksimcthuk@gmail.com>
2026-06-10 08:53:05 +0300

    fix: No logging or debugging functions

--- stdlib.bash
+++ stdlib.bash
@@ -30,3 +30,28 @@ function is_alphanumeric_dash_underscore ()
   [[ $1 =~ ^[A-Za-z0-9_-]+$ ]]
 }
 
+COLOR_RESET="\033[0m"
+COLOR_INFO="\033[1;34m"    # Bold Blue
+COLOR_WARN="\033[1;33m"    # Bold Yellow
+COLOR_ERROR="\033[1;31m"   # Bold Red
+
+function log_info ()
+{
+  echo -e "${COLOR_INFO}[INFO]  $*${COLOR_RESET}";
+}
+
+function log_warn ()
+{
+  echo -e "${COLOR_WARN}[WARN]  $*${COLOR_RESET}" >&2;
+}
+
+function log_error ()
+{
+  echo -e "${COLOR_ERROR}[ERROR]  $*${COLOR_RESET}" >&2;
+}
+
+function message ()
+{
+  echo -e "$*"
+}
+
